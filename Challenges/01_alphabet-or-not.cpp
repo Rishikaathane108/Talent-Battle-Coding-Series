8af1525862342e792f59c9a3b290ded3fcce4ca0
Rishikaathane108 <91424358+Rishikaathane108@users.noreply.github.com>
2023-08-11 23:19:59 +0530

    Update 01_alphabet-or-not.cpp

--- Challenges/01_alphabet-or-not.cpp
+++ Challenges/01_alphabet-or-not.cpp
@@ -1,1 +1,21 @@
-#Cpp Code 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a;
+
+    cout<<"Enter Character : ";
+    cin>>a;
+
+    if(('a'>=97 && 'a'<=122) || ('a'>=65 && 'a'<=90) )
+    {
+        cout<<endl<<"Alphabet";
+    }
+    else
+    {
+        cout<<endl<<"Not alphabet";
+    }
+
+    return 0;
+}
